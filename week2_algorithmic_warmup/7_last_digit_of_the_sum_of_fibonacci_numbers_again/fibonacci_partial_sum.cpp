9ae3131b841e0510d08f48829791c5e1f50109a0
Valentin Popov <valeo.popov@gmail.com>
2018-05-25 15:01:43 +0300

    fibonacci_partial_sum.cpp implemented

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <cassert>
+
 using std::vector;
 
 long long get_fibonacci_partial_sum_naive(long long from, long long to) {
@@ -21,8 +24,47 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+// use Pisano period
+int get_fibonacci_partial_sum_fast(long long from, long long to) {
+
+    const int m = 10;
+
+    std::vector<long long> sequence(6 * m);
+    sequence[0] = 0;
+    sequence[1] = 1;
+
+    int cnt = 1;
+    for (int i = 2; i <= 6 * m; ++i) {
+        sequence[i] = (sequence[i-2] + sequence[i-1]) % m;
+        ++cnt;
+        if (sequence[i-1] == 0 && sequence[i] == 1) {
+            // size has 2 elements from next sequence
+            cnt -= 2;
+            break;
+        }
+    }
+
+    int sum = 0;
+    // sum sequence to n % cnt position
+    for (int i = from; i <= to % cnt; ++i) {
+        sum += sequence[i];
+    }
+
+    return sum % m;
+
+}
+
+void test_solution() {
+    for (int n = 0; n < 10; ++n) {
+        int a = rand() % 50;
+        int b = rand() % 40;
+        assert(get_fibonacci_partial_sum_naive(b, a) == get_fibonacci_partial_sum_fast(b, a));
+    }
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    std::cout << get_fibonacci_partial_sum_fast(from, to) << '\n';
+    //test_solution();
 }
