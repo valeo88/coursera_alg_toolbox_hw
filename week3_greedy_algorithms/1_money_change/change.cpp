22759e782f49ef884a1113ec724a9ccb7b510fe2
Valentin Popov <valeo.popov@gmail.com>
2018-05-27 08:26:02 +0300

    money change is implemented

--- week3_greedy_algorithms/1_money_change/change.cpp
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
 int get_change(int m) {
-  //write your code here
-  return n;
+  //we have coins with denominations of 1,5,10
+  // need to find minimum of coins to change m value of money
+  // for the first move we need to use maximum possible denom coin
+  const int coin_denoms[] = {10,5,1};
+  int cnt = 0;
+  for (int i = 0; i < 3; ++i) {
+    if (m < coin_denoms[i]) {
+        continue;
+    } else {
+        const int reminder = m % coin_denoms[i];
+        cnt += (m - reminder) / coin_denoms[i];
+        m = reminder;
+        if (m == 0) {
+            break;
+        }
+    }
+  }
+
+  return cnt;
 }
 
 int main() {
