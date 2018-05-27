080626c41a14266d7cdddb6e07c1b5dacbf43e21
Valentin Popov <valeo.popov@gmail.com>
2018-05-27 09:07:27 +0300

    knapsack is implemented

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <utility>
 
 using std::vector;
+using std::sort;
+using std::find;
+using std::distance;
+using std::min;
+using std::pair;
+using std::make_pair;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
 
   // write your code here
+  // create vector of pairs (weight, value)
+  vector<pair<int,int>> x;
+  for (int i = 0; i < weights.size(); ++i) {
+    x.push_back(make_pair(weights[i], values[i]));
+  }
+  // sort weights in increasing order or v/w
+  sort(x.begin(), x.end(), [](const auto& p1, const auto& p2) {
+    return static_cast<double>(p1.second) / p1.first < static_cast<double>(p2.second) / p2.first;
+  });
+  //for (const auto& p : x) {
+  //  std::cout << p.first << " ";
+  //}
+  // fill the knapsack in iteration in decreasing order
+  for (int i = x.size() - 1; i >= 0; --i) {
+    if (capacity == 0) break;
+    int a = min(x[i].first, capacity);
+    value += a * static_cast<double>(x[i].second) / x[i].first;
+    capacity -= a;
+  }
 
   return value;
 }
