6fbaa7f99bd3bb81929ae2162de312dcc1cc8325
Valentin Popov <valeo.popov@gmail.com>
2018-05-28 10:53:59 +0300

    max salary is implemented

--- week3_greedy_algorithms/6_maximum_salary/largest_number.cpp
+++ week3_greedy_algorithms/6_maximum_salary/largest_number.cpp
@@ -6,9 +6,14 @@
 
 using std::vector;
 using std::string;
+using std::sort;
 
 string largest_number(vector<string> a) {
   //write your code here
+  // all we need is to sort strings in decreasing order, i.e 21 must be less than 2
+  sort(a.begin(), a.end(), [](const string& s1, const string& s2) {
+    return s1 + s2 > s2 + s1;
+  });
   std::stringstream ret;
   for (size_t i = 0; i < a.size(); i++) {
     ret << a[i];
