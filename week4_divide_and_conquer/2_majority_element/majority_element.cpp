90504085a50d9ea6291c5e1f6da3d1b1669d1a8f
Valentin Popov <valeo.popov@gmail.com>
2018-05-30 20:52:15 +0300

    majority element is implemented

--- week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -8,7 +8,29 @@ int get_majority_element(vector<int> &a, int left, int right) {
   if (left == right) return -1;
   if (left + 1 == right) return a[left];
   //write your code here
-  return -1;
+  // split vector in the middle
+  int half_size = (right - left) / 2;
+  int mid = left + half_size;
+  // get majority elements of each part of the vector
+  int b = get_majority_element(a, left, mid);
+  int c = get_majority_element(a, mid, right);
+  //std::cout <<  "mid = " << mid << ": b = " << b << " c = " << c << "\n";
+  // if elements is equal - we don't need to count it
+  if (b == c) return b;
+
+  // count numbers of b and c in this part of vector a
+  int count_b = std::count(a.begin()+left, a.begin()+right, b);
+  int count_c = std::count(a.begin()+left, a.begin()+right, c);
+
+  // we need to select only potential majority element, i.e. count > half_size of current part of vector a
+  // becase if exists majority element, then exist parts of vector when it's major
+  if (count_b > half_size) {
+    return b;
+  } else if (count_c > half_size) {
+    return c;
+  } else {
+    return -1;
+  }
 }
 
 int main() {
