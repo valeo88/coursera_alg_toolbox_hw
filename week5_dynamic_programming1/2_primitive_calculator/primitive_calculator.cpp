aa96d4cde302c2e9ab8f8318ba70c0d77e29b714
Valentin Popov <valeo.popov@gmail.com>
2018-06-03 18:01:51 +0300

    primitive calc is implemented

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 using std::vector;
+using std::min;
 
 vector<int> optimal_sequence(int n) {
   std::vector<int> sequence;
@@ -23,18 +25,49 @@ vector<int> optimal_sequence(int n) {
 // with dynamic programming
 vector<int> optimal_sequence_dp(int n) {
   std::vector<int> sequence;
-  while (n >= 1) {
+  // count min steps for every number from 1 to n
+  std::vector<int> steps(n+1);
+  steps[1] = 0;
+  steps[2] = 1;
+  steps[3] = 1;
+  for (int i = 4; i < steps.size(); ++i) {
+    int i_div_3 = std::numeric_limits<int>::max();
+    int i_div_2 = std::numeric_limits<int>::max();
+    int i_minus_1 = steps[i - 1] + 1;
+    if (i % 3 == 0) {
+        i_div_3 = steps[i/3] + 1;
+    }
+    if (i % 2 == 0) {
+        i_div_2 = steps[i/2] + 1;
+    }
+    // find minimum steps count for i
+    steps[i] = min(i_minus_1, min(i_div_3, i_div_2));
+  }
+  // decrease n using info from steps
+  while (n > 0) {
     sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if ((n - 1) % 3 == 0) {
-      n--;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else if ((n - 1) % 2 == 0) {
-      n--;
-    } else {
-      n--;
+    if(n % 2 == 0 && n % 3 != 0) {
+        if (steps[n-1] < steps[n/2]) {
+            n--;
+        } else {
+            n /= 2;
+        }
+    } else if(n % 2 != 0 && n % 3 == 0) {
+        if(steps[n-1] < steps[n/3]) {
+            n--;
+        } else {
+            n /= 3;
+        }
+    } else if (n % 2 == 0 && n % 3 == 0) {
+        if (steps[n/3] <= steps[n/2] && steps[n/3] <= steps[n-1]) {
+            n /= 3;
+        } else if (steps[n/2] <= steps[n/3] && steps[n/2] <= steps[n-1]) {
+            n /= 2;
+        } else {
+            n--;
+        }
+    } else if (n % 2 != 0 && n % 3 != 0) {
+        n--;
     }
   }
   reverse(sequence.begin(), sequence.end());
