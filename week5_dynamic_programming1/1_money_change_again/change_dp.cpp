d108b7c9d2be0e21e5caa2d870334f43d8f57639
Valentin Popov <valeo.popov@gmail.com>
2018-06-03 08:39:54 +0300

    money change is implemented

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <limits>
+
+using std::vector;
+using std::min;
 
 int get_change(int m) {
   //write your code here
-  return m / 4;
+  vector<int> changes(m+1);
+  changes[0] = 0;
+  const vector<int> coins = {1,3,4};
+  for (int i = 1; i < changes.size(); ++i) {
+    changes[i] = std::numeric_limits<int>::max();
+    for (int coin : coins) {
+        if (i >= coin) {
+            int num_coins = changes[i - coin] + 1;
+            if (num_coins < changes[i]) {
+                changes[i] = num_coins;
+            }
+            //std::cout << "i= " << i << " coin= " << coin << " num_coins=" << num_coins << " changes[i]=" << changes[i] << "\n";
+        }
+    }
+  }
+
+  return changes[m];
 }
 
 int main() {
