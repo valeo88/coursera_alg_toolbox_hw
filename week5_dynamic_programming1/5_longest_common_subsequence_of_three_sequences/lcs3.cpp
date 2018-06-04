81d5413aaefca3c55c1c3ae295d08b78adc71cd1
Valentin Popov <valeo.popov@gmail.com>
2018-06-04 08:14:56 +0300

    longest common subsequence of 3 vectors is implemented

--- week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
+++ week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
@@ -2,10 +2,38 @@
 #include <vector>
 
 using std::vector;
+using std::max;
 
 int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
   //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+  const size_t n = a.size();
+  const size_t m = b.size();
+  const size_t k = c.size();
+  int l[n+1][m+1][k+1];
+  // iterate over 3 vectors from the start of it
+  for (int i = 0; i <= n; ++i) {
+    for (int j = 0; j <= m; ++j) {
+        for (int p = 0; p <= k; ++p) {
+            if (i == 0 || j == 0 || p == 0) {
+                // first row and first column are zero
+                l[i][j][p] = 0;
+            } else if (a[i-1] == b[j-1] && a[i-1] == c[p-1]) {
+                // current elements is equal - add to subsequence lenght
+                l[i][j][p] = l[i-1][j-1][p-1] + 1;
+            } else {
+                // current elements is not equal - calc max of subsequences without first element and second element
+                const int m1 = max(l[i-1][j][p], l[i][j-1][p]);
+                const int m2 = max(l[i][j][p-1], l[i][j-1][p-1]);
+                const int m3 = max(l[i-1][j-1][p], l[i-1][j][p-1]);
+                l[i][j][p] = max(m1, max(m2, m3));
+            }
+            //std::cout << l[i][j] << " ";
+        }
+    }
+    //std::cout << "\n";
+  }
+  return l[n][m][k];
+  //return std::min(std::min(a.size(), b.size()), c.size());
 }
 
 int main() {
